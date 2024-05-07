43686395545d5cff948e32791916e9ba541e2a45
PRINCE ADEN <princeaden1@gmail.com>
2024-05-07 11:44:56 +0100

    api

--- database.php
+++ database.php
@@ -1,18 +1,15 @@
 <?php
 class Database{
-  
     // specify your own database credentials
     private $host = "localhost";
-    private $db_name = "api_db";// database
-    private $username = "root";
-    private $password = "";
+    private $db_name = "api_db";// database name
+    private $username = "root"; // databaese user name
+    private $password = ""; // database password
     public $conn;
   
     // get the database connection
     public function getConnection(){
-  
         $this->conn = null;
-  
         try{
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->exec("set names utf8");
